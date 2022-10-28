036d77813a8caae0e6440bfbcdfabbd90b3a30e8
Dmitry <103439106+Solmyrik@users.noreply.github.com>
2022-10-28 12:10:12 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -18,6 +18,49 @@ if (iconMenu) {
   });
 }
 
+const objTest = {
+  number: '',
+  name: null,
+  inputWhereData: '',
+  inputHowData: '',
+  inputInstallData: '',
+  inputGroundData: '',
+  inputLevelData: '',
+  inputWantData: '',
+};
+
+function redioCheck(item) {
+  let data;
+  for (let i = 0; i < item.length; i++) {
+    if (item[i].checked) {
+      data = item[i].value;
+      break;
+    }
+  }
+  return data;
+}
+
+function sendMessage() {
+  const testName = document.getElementById('test__name');
+  const testNumber = document.getElementById('test__number');
+  const inputWhere = document.querySelectorAll('.input-whare');
+  const inputHow = document.querySelectorAll('.input-how');
+  const inputInstall = document.querySelectorAll('.input-install');
+  const inputGround = document.querySelectorAll('.input-ground');
+  const inputLevel = document.querySelectorAll('.input-level');
+  const inputWant = document.querySelectorAll('.input-want');
+
+  objTest.number = testNumber.value;
+  objTest.name = testName.value;
+  objTest.inputWhereData = redioCheck(inputWhere);
+  objTest.inputHowData = redioCheck(inputHow);
+  objTest.inputInstallData = redioCheck(inputInstall);
+  objTest.inputGroundData = redioCheck(inputGround);
+  objTest.inputLevelData = redioCheck(inputLevel);
+  objTest.inputWantData = redioCheck(inputWant);
+}
+
+sendMessage();
 const nextBtn = document.querySelector('.test__btn_next');
 const prewBtn = document.querySelector('.test__btn_prew');
 const testItems = document.querySelectorAll('.test__item');
@@ -25,7 +68,12 @@ const percentItem = document.querySelector('.test__percent');
 const titleTest = document.querySelector('.test__title');
 let step = 0;
 nextBtn.addEventListener('click', () => {
-  if (step < testItems.length) {
+  console.log(step, testItems.length);
+  if (step + 1 === testItems.length) {
+    sendMessage();
+    return;
+  }
+  if (step < testItems.length - 1) {
     testItems[step].classList.remove('_active');
     step++;
     testItems[step].classList.add('_active');
@@ -50,3 +98,55 @@ function percent(step) {
   let current = parseInt((100 / heightItems) * (step + 1));
   percentItem.style.width = current + '%';
 }
+
+const installBtn = document.querySelector('.install__btn');
+installBtn.addEventListener('click', feqHandler);
+function feqHandler(e) {
+  console.log(e);
+  e.preventDefault();
+  currentContent = e.target.previousElementSibling;
+  e.target.classList.toggle('active');
+  if (e.target.classList.contains('active')) {
+    currentContent.style.maxHeight = currentContent.scrollHeight + 'px';
+    installBtn.innerText = 'Свернуть';
+  } else {
+    currentContent.style.maxHeight = 160 + 'px';
+    installBtn.innerText = 'Читать далее';
+  }
+}
+
+const catalogBtns = document.querySelectorAll('.catalog__btn');
+const popup = document.querySelector('.popup');
+catalogBtns.forEach((element) => {
+  element.addEventListener('click', () => {
+    popup.classList.add('active');
+    popup.addEventListener('click', (e) => {
+      if (e.target.classList.value === 'popup__wrapper') {
+        popup.classList.remove('active');
+      }
+    });
+  });
+});
+
+const headerElement = document.querySelector('.header');
+const callback = function (entries, observer) {
+  if (entries[0].isIntersecting) {
+    headerElement.classList.remove('_scroll');
+  } else {
+    headerElement.classList.add('_scroll');
+  }
+};
+const headerObserver = new IntersectionObserver(callback);
+headerObserver.observe(headerElement);
+
+const inputBtns = document.querySelectorAll('.input__btn');
+inputBtns.forEach((e) => {
+  e.onclick = () => {
+    alert(e.previousElementSibling.value);
+  };
+});
+// mainBtn.onclick = () => {
+//   return;
+// };
+
+// console.log(mainBtn.previousElementSibling.value);
